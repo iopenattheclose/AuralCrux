98f79aaa652188fa109c06589b7340d7f980b0a5
iopenattheclose <prupro69@gmail.com>
2025-07-16 22:36:50 +0530

    added jsx

---
+++ audio-classifier/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+      <main className="min-h-screen bg-stone-50 p-8">
+      <div className="mx-auto max-w-[50%] bg-red-500"> 
+        <p>Hello</p>
+      </div>
+      </main>
+  );
+}
+
+
+export default App
